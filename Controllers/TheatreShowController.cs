ff47fdd593b95aa3bb033e7dc0f42b65c463d0c6
MayorMayonnaise <xanderbos04@hotmail.com>
2024-10-16 13:42:30 +0200

    added http functionality for Post, Put and Delete in TheatreShows

--- Controllers/TheatreShowController.cs
+++ Controllers/TheatreShowController.cs
@@ -23,4 +23,24 @@ public class TheatreShowController : Controller {
         if (theatreShow != null) return Ok(theatreShow);
         return BadRequest("TheatreShow with this ID does not exist");
     }   
+
+    [HttpPost()]
+    public async Task<IActionResult> PostTheatreShow([FromBody] TheatreShow theatreShow) {
+        bool addedToDB = await theatreShowService.Create(theatreShow);
+        return addedToDB == true ? Ok(theatreShow) : BadRequest("Failed to add TheatreShow to DB. Entry already exists");
+    }
+
+    [HttpPut()]
+    public async Task<IActionResult> UpdateTheatreShow([FromBody] TheatreShow theatreShow, [FromQuery] int theatreShowId) {
+        if (theatreShowId != theatreShow.TheatreShowId) return BadRequest("The given ID and the id of the TheatreShow does not match");
+        bool updatedTheatreShow = await theatreShowService.Update(theatreShow, theatreShowId);
+        return updatedTheatreShow == true ? Ok($"Successfully updated") : BadRequest("Failed to update TheatreShow");
+    }
+
+    [HttpDelete()]
+    public async Task<IActionResult> DeleteTheatreShow([FromQuery] int id) {
+        bool removedFromDB = await theatreShowService.Delete(id);
+        return removedFromDB == true ? Ok($"TheatreShow with id {id} was successfully removed from db") : BadRequest("Failed to remove TheatreShow to DB. Entry does not exist");
+    }
+
 }
--- Services/TheatreShowService.cs
+++ Services/TheatreShowService.cs
@@ -5,34 +5,50 @@ namespace StarterKit.Services;
 
 public class TheatreShowService : ITheatreShowService
 {
-    DbContext dbContext;
-    public TheatreShowService(DbContext dbContext) {
+    DatabaseContext dbContext;
+    public TheatreShowService(DatabaseContext dbContext) {
         this.dbContext = dbContext;
     }
 
     public async Task<bool> Create(TheatreShow theatreShow)
     {
-        dbContext.Add<TheatreShow>(theatreShow)
-        throw new NotImplementedException();
+        var x = dbContext.TheatreShow.FirstOrDefaultAsync(x => x.TheatreShowId == theatreShow.TheatreShowId);
+        if (x != null) return false;
+        await dbContext.TheatreShow.AddAsync(theatreShow);
+        int n = await dbContext.SaveChangesAsync();
+        return n > 0;
+
     }
 
     public async Task<bool> Delete(int theatreShowId)
     {
-        throw new NotImplementedException();
+        TheatreShow x = await dbContext.TheatreShow.FirstOrDefaultAsync(x => x.TheatreShowId == theatreShowId);
+        if (x == null) return false;
+        dbContext.TheatreShow.Remove(x);
+        int n = dbContext.SaveChanges();
+        return n > 0;
     }
 
     public async Task<IEnumerable<TheatreShow>> GetAll()
     {
-        throw new NotImplementedException();
+        return await dbContext.TheatreShow.ToListAsync();
+        // throw new NotImplementedException();
     }
 
     public async Task<TheatreShow> GetById(int theatreShowId)
     {
+        return await dbContext.TheatreShow.FirstOrDefaultAsync(x => x.TheatreShowId == theatreShowId);
         throw new NotImplementedException();
     }
 
     public async Task<bool> Update(TheatreShow theatreShow, int theatreShowId)
     {
+        TheatreShow x = await dbContext.TheatreShow.FirstOrDefaultAsync(x => x.TheatreShowId == theatreShowId);
+        if (x == null) return false;
+        dbContext.Entry(x).CurrentValues.SetValues(theatreShow);
+        int n = dbContext.SaveChanges();
+        return n>0;
         throw new NotImplementedException();
     }
+
 }
