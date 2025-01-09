0595f3e37e01146767fcdad8063370d185a491c5
Vicentoo <160601636+Vicentoo@users.noreply.github.com>
2025-01-09 18:59:11 +0100

    location watcher URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './VincentComponents/login';
+
+const LocationWatcher: React.FC = () => {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    console.log("Navigated to:", location.pathname);
+  }, [location]);
+
+  return null; 
+};
 
 const App: React.FC = () => {
   return (
     <Router>
+      <LocationWatcher />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/adminlogin" element={<Login/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/adminlogin" element={<Login />} />
       </Routes>
     </Router>
   );
 };
 
-export default App
+export default App;
